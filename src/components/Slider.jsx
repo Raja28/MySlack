688bc728950685ef5e8fb9aec00208477e68010b
Raja28 <rajadavid03@gmail.com>
2025-03-23 16:09:18 +0530

    fix: changes images name

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import slider1 from '../assets/slides1.png'
+import slider1 from '../assets/slider1.png'
 import slider2 from '../assets/slider2.png'
 import slider3 from '../assets/slider3.png'
 import slider4 from '../assets/slider4.jpeg'
